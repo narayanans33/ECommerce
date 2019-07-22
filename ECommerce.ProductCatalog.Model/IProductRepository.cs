59f86e7c117ca62b50f5ac8ac67d3867fc94a0a3
Narayanan Subramanian <nasubram@microsoft.com>
2019-07-22 11:37:39 +0530

    Add project files.

---
+++ ECommerce.ProductCatalog.Model/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+///     Interface to store Products in stateful service data store. Needs to implement an Add and a Get. All methods are Async and return a Task
+/// </summary>
+namespace ECommerce.ProductCatalog.Model
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllProducts();
+
+        Task AddProduct(Product product);
+    }
+}
