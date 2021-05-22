102335cd86738ab8d26cab74acba3605a5fec22c
Narayanan <nasubram@microsoft.com>
2021-05-22 18:51:48 +0530

    cart changes

---
+++ Ecommerce.CheckoutService.Model/CheckoutSummary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ecommerce.CheckoutService.Model
+{
+    public class CheckoutSummary
+    {
+        public List<CheckoutProduct> Items { get; set; }
+
+        public double TotalPrice { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
---
+++ Ecommerce.CheckoutService.Model/ICheckoutService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.ServiceFabric.Services.Remoting;
+using System;
+using System.Threading.Tasks;
+
+namespace Ecommerce.CheckoutService.Model
+{
+    public interface ICheckoutService : IService
+    {
+        Task<CheckoutSummary> CheckoutAsync(string userId);
+
+        Task<CheckoutSummary[]> GetOrderHistoryAsync(string userId);
+
+    }
+}
